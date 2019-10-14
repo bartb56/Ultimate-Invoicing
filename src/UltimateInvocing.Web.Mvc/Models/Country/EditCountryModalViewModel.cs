bd68365cdfc24b490efdedaa141af21040b370c3
Bart Blokhuis <0285920@student.rocvantwente.nl>
2019-10-14 21:59:24 +0200

    fixes changing the email

--- src/UltimateInvocing.Web.Mvc/Models/Country/EditCountryModalViewModel.cs
+++ src/UltimateInvocing.Web.Mvc/Models/Country/EditCountryModalViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UltimateInvocing.Attributes;
 using UltimateInvocing.Country.Dto;
 
 namespace UltimateInvocing.Web.Models.Country
@@ -17,6 +18,7 @@ namespace UltimateInvocing.Web.Models.Country
             DisplayOrder = output.DisplayOrder;
         }
         public Guid Id { get; set; }
+        [UltimateResourceDisplayName("patatekes")]
         public string Name { get; set; }
         public string IsoCode { get; set; }
         public string IsoCode3 { get; set; }
